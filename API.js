a8c7f56ab39bfad241641944f94660ff4d097f17
jpark03 <111878997+jpark03@users.noreply.github.com>
2022-09-17 20:20:16 -0400

    Add files via upload

---
+++ API.js
@@ -0,0 +1,38 @@
+async function getApi() {
+
+    const response = await fetch('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/West%20Lafayette?unitGroup=metric&key=N8F2BH4BRLZDNV2HPZPBQ2XWV&contentType=json')
+
+    const obj = await response.json()
+
+    return obj;
+}
+
+var weather = document.getElementById("weatherList");
+(async() => {
+ let x = await getApi();
+ console.log(x);
+ console.log(x.days[0]);
+ console.log(x.address);
+ var item = document.getElementById("location");
+ item.innerHTML = x.address;
+
+ for (var i=0;i<x.days.length;i++) {
+    console.log("DATE: " + x.days[i].datetime + " || CONDITIONS: " + x.days[i].conditions + " || TEMP: " + x.days[i].temp);
+    var data = document.createElement("p");
+    data.innerHTML = "DATE: " + x.days[i].datetime + " || CONDITIONS: " + x.days[i].conditions + " || TEMP: " + x.days[i].temp + "C";
+    weather.appendChild(data);
+ }
+//  var list = document.getElementById("list-group");
+ 
+if (x.days[0].conditions == "Partially cloudy") {
+    document.querySelector('body').style.backgroundImage="url(images/PartiallyCLoud.jpeg)";
+ } else if (x.days[0].conditions == "Rain") {
+    document.querySelector('body').style.backgroundImage="url(images/RainyDay.jpg)";
+ } else if (x.days[0].conditions == 'Overcast') {
+    document.querySelector('body').style.backgroundImage="url(images/Overcast.jpeg)";
+ } else if (x.days[0].conditions == "Rain, Partially cloudy") {
+    document.querySelector('body').style.backgroundImage="url(images/RainCloud.jpeg)";
+ } else {
+    document.querySelector('body').style.backgroundImage="url(images/Clear.jpeg)";
+ }
+})()
